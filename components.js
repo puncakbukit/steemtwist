f4d874fc4a9da50a0c464d70f25c3506751e903e
Puncak Bukit <26822588+puncakbukit@users.noreply.github.com>
2026-03-28 15:18:48 +0900

    Update components.js

--- components.js
+++ components.js
@@ -2531,6 +2531,7 @@ const LiveTwistComponent = {
 <html>
 <head>
 <meta charset="utf-8">
+<meta http-equiv="Content-Security-Policy" content="default-src 'none'; script-src 'unsafe-inline' https://cdnjs.cloudflare.com; style-src 'unsafe-inline'; img-src data: https: http:; connect-src 'none'; frame-src 'none'; object-src 'none'; base-uri 'none';">
 <script src="https://cdnjs.cloudflare.com/ajax/libs/dompurify/3.1.6/purify.min.js" integrity="sha256-wIRQlqfEpnQfNirFBslMHH0n3GA7zBv2Slh/dvLb46E=" crossorigin="anonymous"></script>
 <style>
   body { margin:0; padding:8px; font-family:system-ui,sans-serif;
